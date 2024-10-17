e637fa135957d4a2fe721698067a711fdf9f21d4
KaungMon <kaungmon.dev@example.com>
2024-10-17 23:09:15 +0630

    database migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,13 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
+        'first_name',
+        'surname',
+        'address',
+        'phone',
+        'gender',
+        'user_role_id',
         'email',
         'password',
     ];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
+            $table->string('first_name');
+            $table->string('surname');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('gender');
+            $table->integer('user_role_id');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
