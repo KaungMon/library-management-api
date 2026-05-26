34606645df5c98702b8106d9a34d0001e720bb4f
KaungMon <kaungmon.dev@example.com>
2026-05-27 01:59:55 +0630

    commit

--- routes/api.php
+++ routes/api.php
@@ -7,41 +7,19 @@ use App\Http\Controllers\StatusController;
 use App\Http\Controllers\AuthorsController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\BooksTableController;
-<<<<<<< HEAD
 use App\Http\Controllers\BorrowingsController;
 use App\Http\Controllers\BookCategoryController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\UserController;
-=======
-use App\Http\Controllers\UserController;
-use App\Models\BookCategory;
->>>>>>> 9776e61e27c0487f42089e8a668a85102afb56c8
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-<<<<<<< HEAD
-// SECTION - auth
-Route::group(['prefix' => 'auth'], function () {
-    Route::post('login', [UserController::class, 'login']);
-=======
 // SECTION - user
 Route::group(['prefix' => 'user'], function () {
     Route::post('login', [UserController::class, 'login']);
     Route::post('signup', [UserController::class, 'signup']);
->>>>>>> 9776e61e27c0487f42089e8a668a85102afb56c8
 });
 // !SECTION
 
