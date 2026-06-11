4cd72ae214cc5ae60e1155bdd473a9076f54692e
KaungMon <kaungmon.dev@example.com>
2026-06-11 21:53:58 +0630

    wep : temparary save to rebase logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,40 +6,45 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
     // SECTION - create
-    public function login(Request $request) {
+    public function login(Request $request)
+    {
+
         $credentials = $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required'],
         ]);
 
-        if(Auth::attempt($credentials)) {
+        if (Auth::attempt($credentials, $request->remember_me)) {
             $request->session()->regenerate();
             return response()->json([
                 "message" => "Login successfully",
-                "user" => Auth::user(),
+                "user_id" => Auth::user()->id,
 
             ], 202);
+        } else {
+            return response()->json([
+                "message" => "Invalid email or password."
+            ], 401);
         }
-
-        return response()->json([
-            "message" => "Invalid email or password."
-        ], 401);
     }
     // !SECTION
 
     // SECTION - register
-    public function signup(Request $request) {
+    public function signup(Request $request)
+    {
+
         $data = $this->getData($request);
         $data["user_role_id"] = 1;
         logger($data);
         User::create($data);
         $user = User::where("email", $data["email"])->first();
         logger($user);
-        return response() ->json([
+        return response()->json([
             "user" => $user,
             "token" => $user->createToken(time())->plainTextToken,
         ]);
@@ -47,7 +52,8 @@ class UserController extends Controller
     // !SECTION
 
     // SECTION - get data
-    private function getData($request) {
+    private function getData($request)
+    {
         return [
             "first_name" => $request->firstName,
             "surname" => $request->surname,
--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,9 @@ use App\Http\Controllers\UserController;
 
 use Illuminate\Http\Request;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->prefix('user')->group(function () {
+    Route::get('info', [UserController::class, 'info']);
+    Route::get('logout', [UserController::class, 'logout']);
 });
 
 // SECTION - user
